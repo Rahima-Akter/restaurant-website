27046608d366b231153822a601ecda5718b3ecfa
Rahima-Akter <rahimaakter1527@gmail.com>
2024-03-22 09:49:46 -0700

    this is a push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  content: ['*'],
+  content: ["*"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1200px',
+    },
     extend: {
       backgroundImage: {
         'hero-bg': "linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url(img/g4.jpeg);",
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
 }
