0f07b697c40ede8c9503dcae720d9f685da3a1ee
Rahima-Akter <rahimaakter1527@gmail.com>
2024-03-25 09:12:43 -0700

    this is a push

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero-bg': "linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url(img/g4.jpeg);",
+        'banner-bg': "linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url(img/food-banner.jpg);",
       }
     },
   },
